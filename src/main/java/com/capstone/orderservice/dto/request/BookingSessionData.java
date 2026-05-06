5d07a51e1fb7f3bd61c3f3d7284b3fdfbdfab857
KuaBK <tuanphonglqd@gmail.com>
2026-05-06 22:24:38 +0700

    update bookingSessionData

--- src/main/java/com/capstone/orderservice/dto/request/BookingSessionData.java
+++ src/main/java/com/capstone/orderservice/dto/request/BookingSessionData.java
@@ -1,12 +1,17 @@
 package com.capstone.orderservice.dto.request;
 
 import lombok.Data;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 public class BookingSessionData {
     private String userId;
     private List<BookingItem> items;
+    private String eventName;
+    private LocalDateTime time;
+    private String venue;
 
     @Data
     public static class BookingItem {
