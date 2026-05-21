9898ad23f3ee62264d0d65ad360c8bba179f4fd3
KuaBK <tuanphonglqd@gmail.com>
2026-05-21 20:22:15 +0700

    refactor

---
+++ src/main/java/com/capstone/orderservice/client/BinCodeDto.java
@@ -0,0 +1,12 @@
+package com.capstone.orderservice.client;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BinCodeDto {
+    String binCode;
+}
--- src/main/java/com/capstone/orderservice/client/InventoryFeignClient.java
+++ src/main/java/com/capstone/orderservice/client/InventoryFeignClient.java
@@ -31,5 +31,5 @@ public interface InventoryFeignClient {
     boolean getAllowResale(@PathVariable Long eventId);
 
     @GetMapping("/bank/bin-code")
-    BaseResponse<String> getBinCodeFromBankCode(@RequestParam String bankCode);
+    BaseResponse<BinCodeDto> getBinCodeFromBankCode(@RequestParam String bankCode);
 }
